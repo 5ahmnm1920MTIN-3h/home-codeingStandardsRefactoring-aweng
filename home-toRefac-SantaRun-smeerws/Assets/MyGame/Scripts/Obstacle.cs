ce5b2f80071f39294be9de6c38859ac78a83b379
aweng <annawenger@gmx.at>
2019-12-16 22:34:31 +0100

    deleted unused using, deleted unneccasary empty lines

--- home-toRefac-SantaRun-smeerws/Assets/MyGame/Scripts/Obstacle.cs
+++ home-toRefac-SantaRun-smeerws/Assets/MyGame/Scripts/Obstacle.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
@@ -12,13 +10,6 @@ private void Awake()
     rb = GetComponent<Rigidbody2D>();
 }
 
-// Start is called before the first frame update
-void Start()
-{
-        
-}
-
-// Update is called once per frame
 void Update()
 {
     //if obstacle's position x is < -15f it will be destroyed
@@ -31,14 +22,10 @@ void Update()
         {
             Destroy(gameObject);
         }
-
     }
-
-
 
 private void FixedUpdate()
 {
-
     rb.velocity = Vector2.left * MoveSpeed;
 
 }
