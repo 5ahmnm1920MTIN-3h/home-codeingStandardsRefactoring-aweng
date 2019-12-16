990d21ef5f296a17e2eb2c62c4c483900fcba495
aweng <annawenger@gmx.at>
2019-12-16 22:45:06 +0100

    deleted unneccasary using and empty lines, as well as empty method

--- home-toRefac-SantaRun-smeerws/Assets/MyGame/Scripts/ObstacleSpawner.cs
+++ home-toRefac-SantaRun-smeerws/Assets/MyGame/Scripts/ObstacleSpawner.cs
@@ -1,17 +1,12 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
     public static ObstacleSpawner instance;
-
     public GameObject[] obstacles;
-
     public bool gameOver = false;
-
     public float minSpawnTime, maxSpawnTime;
-
 
     private void Awake()
     {
@@ -20,31 +15,20 @@ public class ObstacleSpawner : MonoBehaviour
             instance = this;
         }
     }
-
-    // Start is called before the first frame update
+    
     void Start()
     {
         StartCoroutine("Spawn");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     IEnumerator Spawn()
     {
         float waitTime = 1f;
-
         yield return new WaitForSeconds (waitTime);
-
         while (!gameOver)
         {
             SpawnObstacle();
-            
             waitTime = Random.Range(minSpawnTime,maxSpawnTime);
-
             yield return new WaitForSeconds(waitTime);
         }
     }
@@ -52,7 +36,6 @@ public class ObstacleSpawner : MonoBehaviour
     void SpawnObstacle()
     {
         int random = Random.Range(0,obstacles.Length);
-
         Instantiate(obstacles[random],transform.position,Quaternion.identity);
     }
 }
